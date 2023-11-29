1f159f55481a77bf43b0b487af975b4cf5551584
Ivanovicx Nuñez <ivan25606@gmail.com>
2023-11-29 10:25:06 -0600

    Initial Commit

---
+++ 2023_11_29_appGraficos2D/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2023_11_29_appGraficos2D
+//
+//  Created by Ivanovicx Nuñez on 29/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
