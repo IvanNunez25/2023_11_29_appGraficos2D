3d4e46bb7af3958d19c0569bae95f6a6756878e0
Ivanovicx Nuñez <ivan25606@gmail.com>
2023-11-29 10:27:06 -0600

    Base del proyecto

---
+++ 2023_11_29_appGraficos2D/Graficos2D.swift
@@ -0,0 +1,20 @@
+//
+//  Graficos2D.swift
+//  2023_11_29_appGraficos2D
+//
+//  Created by Ivanovicx Nuñez on 29/11/23.
+//
+
+import UIKit
+
+class Graficos2D: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
